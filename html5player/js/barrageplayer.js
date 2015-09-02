335ebc8ca227ee85631feb6aea4cd15fcc168da3
yyd <yueyongdong@gmail.com>
2015-09-02 16:02:50 +0800

    separated barrage to barrage/1.js, modified js/barrageplayer.js to display barrage common... a little problem...

--- html5player/js/barrageplayer.js
+++ html5player/js/barrageplayer.js
@@ -9,9 +9,39 @@ var player = function () {
 		videoCanvas = document.getElementById("video").getContext('2d'),
 		videoId,
 		videoHiding = true,
+		maxBarrageHeight = 30,
 		timeLine = document.getElementById('timeline').getContext('2d'),
 		barrage = document.getElementById('barrage').getContext('2d'),
-		barragePool = [],
+		// 弹幕池内容为x, y, type, color, speed, content
+		barragePool = [], 
+		// 通道的最大值
+		channelStatus = [0,0,0,0,0,0,0,0,0,0,
+						0,0,0,0,0,0,0,0,0,0],
+		getAvaliableChannel = function(speed) {
+			for (var i = 1; i < Math.floor(videoElementHeight / maxBarrageHeight); ++i) {
+				// 该通道为空的情况
+				if (channelStatus[i] == 0) {
+					channelStatus[i] += 1;
+					return i;
+				}
+				// 通道不为空的情况
+				var maxX = 0,
+					speedWithMaxX = 0.5;
+				for (var j = 0; j < barragePool.length; ++j) {
+					if ((barragePool[j].y / maxBarrageHeight) == i ) {// 先判断是否是同一个通道
+						if (barragePool[j].x + barrage.measureText(barragePool[j].content).width > maxX) {// 再计算先前的弹幕右端到屏幕左边的时间是否比后面的弹幕左端到屏幕左边的时间短
+							maxX = barragePool[j].x + barrage.measureText(barragePool[j].content).width;
+							speedWithMaxX = barragePool[j].speed;
+						}
+					}
+				}
+				if ((maxX / speedWithMaxX) <= (videoWidth / speed)) {
+					console.log('barrage before: ' + maxX + ' with speed ' + speedWithMaxX + '; barrage after with speed ' + speed);
+					channelStatus[i] += 1;
+					return i;
+				}
+			}
+		}
 		// 格式化时间
 		secondsFormat = function (sec) {
 			var rawTime = Math.floor(sec),
@@ -21,7 +51,7 @@ var player = function () {
 		},
 		// 添加弹幕
 		addBarrageToPool = function (barrageArray) {
-			for (var i = 1; i < barrageArray.length; ++i) {
+			for (var i = barrageArray.length - 1; i > 0; --i) {
 				if (barrageArray[i] == "#") {
 					console.log('delete ' + barrageArray[i] + ' from barrage array...');
 					barrageArray.splice(i, 1);
@@ -30,14 +60,16 @@ var player = function () {
 			for (var i = 1; i < barrageArray.length; ++i) {	
 				var barrageElement = barrageArray[i].split(',');
 				if (barrageElement[0] < videoSrc.currentTime) {
-					var barrageObj = {
-						x: 800,
-						y: 25,
-						type: barrageElement[1],
-						color: barrageElement[4],
-						speed: barrage.measureText(barrageElement[6]).width / 50,
-						content: barrageElement[6],
-					};
+					var barrageSpeed = barrage.measureText(barrageElement[6]).width / 50,
+						barrageObj = {
+							x: videoWidth,
+							y: getAvaliableChannel(barrageSpeed) * maxBarrageHeight, // maxBarrageHeight为最大的弹幕高度
+							type: barrageElement[1],
+							size: barrageElement[2] <= maxBarrageHeight ? barrageElement[2] : maxBarrageHeight,
+							color: barrageElement[4],
+							speed: barrageSpeed,
+							content: barrageElement[6],
+						};
 					barragePool.push(barrageObj);
 					console.log("add " + barrageObj.content + " width " + barrage.measureText(barrageElement[6]).width + " speed " + barrageObj.speed);
 					// 要删除的弹幕赋值为‘#’
@@ -48,16 +80,22 @@ var player = function () {
 		// 移动弹幕
 		moveBarrage = function () {
 			barrage.clearRect(0, 0, 800, 600);
+			for (var i = barragePool.length - 1; i >= 0; --i) {
+				if (barragePool[i].content == '#') {
+					console.log('delete ' + barragePool[i].content + " from barrage pool.." + "The number of barrage in channel " + (barragePool[i].y / maxBarrageHeight) + " is " + (channelStatus[barragePool[i].y / maxBarrageHeight] - 1) + '...');
+					channelStatus[barragePool[i].y / maxBarrageHeight] -= 1;
+					barragePool.splice(i, 1);
+				}
+			}
 			for (var i = 0; i < barragePool.length; ++i) {
 				if (barragePool[i].type == '1') {
-					barrage.font = '25px 微软雅黑';
+					barrage.font = barragePool[i].size + 'px 微软雅黑';
 					barrage.fillStyle = barragePool[i].color;
 					barrage.fillText(barragePool[i].content, barragePool[i].x, barragePool[i].y);
 				}
 				barragePool[i].x -= barragePool[i].speed;
 				if (barragePool[i].x + barrage.measureText(barragePool[i].content).width < 0) {
-					console.log('delete ' + barragePool[i].content + " from barrage pool..");
-					barragePool.splice(i, 1);
+					barragePool[i].content = "#";
 				}
 			}
 		},
@@ -65,6 +103,7 @@ var player = function () {
 		drawFrame = function () {
 			videoCanvas.drawImage(videoSrc, 0, (videoElementHeight - videoHeight) / 2, videoWidth, videoHeight);
 			videoPlayTime.innerText = secondsFormat(videoSrc.currentTime) + '/' + secondsFormat(videoSrc.duration);
+			timeLine.fillStyle = '#999';
 			timeLine.fillRect(0, 0, videoSrc.currentTime / videoSrc.duration * document.getElementById('timeline').width, document.getElementById('timeline').height);
 			addBarrageToPool(danmuku);
 			moveBarrage();
@@ -120,11 +159,6 @@ var player = function () {
 		}
 		return that;
 	},
-	p = player(),
-	danmuku = ['time, type, size, user, color, timestamp, content',
-		'1.5,1,25,test,#FFFFFF,2015-09-01 09:21,咦！！！！',
-		'6.25,1,25,test,#EEEEEE,2015-09-01 09:43,嗯我想想',
-		'6.75,1,25,test,#EFABCD,2015-09-01 09:43,这这这',
-		'8,1,25,test,#FEDCBA,2015-09-01 15:42,简直太可怕',
-		'10,1,15,test,'];
+	p = player();
+	
 	
