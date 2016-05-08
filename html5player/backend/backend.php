8e64cc1d784c6a89ba01ba444b292b71a087ec9a
yyd <yueyongdong@gmail.com>
2016-05-08 16:17:14 +0800

    a little modify in the backend.php about the barrage check...

--- html5player/backend/backend.php
+++ html5player/backend/backend.php
@@ -7,6 +7,10 @@ $config = array(
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$post = file_get_contents("php://input"); // $post变量为1.mp4||1.5,1,25,test,#FFFFFF,2015-09-01 09:21,咦！！！！这种形式
 	$data = explode("||", $post, 2); //将POST数据分割成两部分 文件名与弹幕 如1.mp4和1.5,1,25,test,#FFFFFF,2015-09-01 09:21,咦！！！！
+	$barrageElements = explode(",", $data[1]);
+	if (count($barrageElements) != 7))) {
+		die('Length wrong?');
+	}
 	$config['barrageFile'] = '../barrage/' . explode(".", $data[0])[0] . '.xml'; //$config['barrageFile'] = "'../barrage/' . '1' . '.xml'"
 	$barrageFile = fopen($config['barrageFile'], "r+") or die('Unable to open file...');
 	// 移动指针至结尾处
@@ -14,5 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$barrage = ",\r\n\t\t'" . $data[1] . "'];"; // 将$data[1]作为弹幕写入文件
 	fwrite($barrageFile, $barrage);
 	fclose($barrageFile);
+} else {
+	echo 'It seems that the METHOD is wrong...';
 }
 ?>
