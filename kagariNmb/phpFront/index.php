a9a05fce18df3f7b93f578c2fc7b3bfc92c64ff7
xubeiyan <yueyongdong@gmail.com>
2016-07-04 20:36:24 +0800

    add .htaccess...

--- kagariNmb/phpFront/index.php
+++ kagariNmb/phpFront/index.php
@@ -2,10 +2,11 @@
 /**
 * 所有访问的入口
 */
-
+//print_r($_GET);
 // 未提交任何GET参数则认为访问主页
-if (!isset($_GET)) {
-	
+if ($_SERVER['QUERY_STRING'] == '') {
+	echo file_get_contents('html/index.html');
+	exit();
 }
 
 if ($_GET['q']) {
